097e44e73b892d92e28f396d4abc37ae32a70f7f
David Francisco Perez Gallegos <dperez10@utmachala.edu.ec>
2026-05-31 23:04:50 -0500

    Error

--- Entidades/CuentaAhorro.cs
+++ Entidades/CuentaAhorro.cs
@@ -8,7 +8,7 @@ namespace SistemaBancario.Entidades
 {
     public class CuentaAhorro : Cuenta
     {
-        public override bool AperturaCuenta() => Saldo >= 99;
+        public override bool AperturaCuenta() => Saldo >= 0;
 
         public override void PagarInteres()
         {
